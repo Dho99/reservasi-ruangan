6dbc2f8623bf3226e2bbdeced270bc4034f24262
Dho99 <ridhoawwaludin@gmail.com>
2025-11-24 14:38:22 +0700

    add api's

---
+++ app/api/blocked-room/route.ts
@@ -0,0 +1,74 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const blockedRooms = await prisma.blockedSlot.findMany();
+
+    return NextResponse.json(
+      {
+        data: blockedRooms,
+      },
+      { status: 200 }
+    );
+  } catch (err: unknown) {
+    return NextResponse.json(
+      {
+        message: err instanceof Error ? err.message : "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const { roomId, waktuMulai, waktuSelesai, alasan } = await request.json();
+
+    const newBlockedRoom = await prisma.blockedSlot.create({
+      data: {
+        roomId,
+        waktuMulai: new Date(waktuMulai).toDateString(),
+        waktuSelesai: new Date(waktuSelesai).toDateString(),
+        alasan,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        data: newBlockedRoom,
+      },
+      { status: 201 }
+    );
+  } catch (err: unknown) {
+    return NextResponse.json(
+      {
+        message: err instanceof Error ? err.message : "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const { roomId } = await request.json();
+    const deleteBlockedRoom = await prisma.blockedSlot.delete({
+      where: {
+        id: roomId
+      }
+    });
+
+    return NextResponse.json({
+      message: "Blocked room entry deleted successfully",
+      data: deleteBlockedRoom
+    }, {status: 200})
+  } catch (err: unknown) {
+    return NextResponse.json(
+      {
+        message: err instanceof Error ? err.message : "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
